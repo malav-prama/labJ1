33f3c684261c50a4dc3d5dc5609a71d081b10a27
malav-prama <101901228+malav-prama@users.noreply.github.com>
2022-08-03 14:56:12 -0700

    Update UserInfo.java

--- src/main/java/com/example/labj1/model/UserInfo.java
+++ src/main/java/com/example/labj1/model/UserInfo.java
@@ -2,19 +2,9 @@ package com.example.labj1.model;
 import javax.persistence.*;
 
 public class UserInfo {
-    @Id
-
-    private Integer id;
+  
     private String name;
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
