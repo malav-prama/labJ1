22c2183c852bcc3f9e67e67cd3c639a0c9036eee
malav-prama <malav.thakker@prama.ai>
2022-08-09 05:38:24 -0700

    http status logic fixed

--- src/main/java/com/example/labj1/controller/OrderController.java
+++ src/main/java/com/example/labj1/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.labj1.controller;
 import com.example.labj1.logic.OrderLogic;
 import com.example.labj1.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,10 @@ public class OrderController {
      OrderLogic orderLogic;
 
      @PostMapping("v1/add/")
-     public void placeOrder(@RequestBody Order Order)
+     public ResponseEntity<?> placeOrder(@RequestBody Order Order)
      {
-         orderLogic.createOrder(Order);
+        return orderLogic.createOrder(Order);
+
      }
 
 
--- src/main/java/com/example/labj1/logic/OrderLogic.java
+++ src/main/java/com/example/labj1/logic/OrderLogic.java
@@ -3,10 +3,7 @@ import com.example.labj1.dao.OrderDao;
 import com.example.labj1.model.Inventory;
 import com.example.labj1.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriBuilder;
@@ -27,15 +24,13 @@ public class OrderLogic {
 
 
     @Transactional
-    public void createOrder(Order order) {
+    public ResponseEntity<?> createOrder(Order order) {
         System.out.println("order: " + order);
         String inventoryUrl = "http://localhost:8081/inventory/v2";
 
         UriComponentsBuilder url = UriComponentsBuilder.fromUriString(inventoryUrl)
 
                 .queryParam("productId", (order.getLineItems().get(0).getProductInfo().getId()));
-        // .queryParam("productId", 24234);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
@@ -43,10 +38,16 @@ public class OrderLogic {
         Inventory inventory = restTemplate.exchange(url.build().toUri().toString(), HttpMethod.GET, entity, Inventory.class).getBody();
         System.out.println("inventory data :: " + inventory.getTotalQuantity());
         if (order.getLineItems().get(0).getProductInfo().getId() == inventory.getProductId() && order.getLineItems().get(0).getQuantity() <= inventory.getTotalQuantity()) {
-            System.out.print("true");
 
-            //orderDao.saveOrder(order);
+            orderDao.saveOrder(order);
+            return ResponseEntity.status(200).build();
+
 
         }
+        else
+            return ResponseEntity.status(406).build();
+
     }
-}
+
+    }
+
