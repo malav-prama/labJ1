9c10c464227616a878b15ebabcc5da979997e8d9
malav-prama <malav.thakker@prama.ai>
2022-08-09 16:51:00 -0700

    updated Inventory Logic

---
+++ src/main/java/com/example/labj1/model/updateInventory.java
@@ -0,0 +1,22 @@
+package com.example.labj1.model;
+
+public class updateInventory {
+    private int productId;
+    private int productQty;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getProductQty() {
+        return productQty;
+    }
+
+    public void setProductQt(int productQty) {
+        this.productQty = productQty;
+    }
+}
